0a296d6bb03e5690fe84aee63ea0f75c03633901
Tallie Elisha <tallieyelisha@gmail.com>
2018-07-17 18:01:52 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,15 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Welcome to your app I am trying to make a change !!!! "
+  end
+  
+  get '/name' do
+    "My name is Tallie"
+  end
+
+get '/boy_time' do
+    "It is now boy_time"
   end
 
 end
